866dffc94d860cbd38c693e15081ace5bf8ee082
irfan44 <irfannmuhajir@gmail.com>
2023-08-26 00:15:44 +0700

    feat: add add item modal

--- src/components/Headers/HomeHeader.vue
+++ src/components/Headers/HomeHeader.vue
@@ -1,11 +1,28 @@
-<script></script>
+<script>
+import AddItemModal from "../Modals/AddItemModal.vue";
+
+export default {
+  components: { AddItemModal },
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+};
+</script>
 
 <template>
   <header>
     <div class="text-center">
-      <h1 class="font-medium text-2xl">Diari Jajan Februari 2021</h1>
+      <h1 class="font-medium text-3xl">Diari Jajan Februari 2021</h1>
       <p class="font-medium text-xl">Pengeluaran Bulan Ini</p>
-      <button class="p-2 bg-indigo-800 text-white mt-4">TAMBAH ITEM</button>
+      <button
+        class="p-2 bg-indigo-800 text-white mt-4"
+        @click="showModal = true"
+      >
+        TAMBAH ITEM
+      </button>
     </div>
   </header>
+  <AddItemModal v-show="showModal" @close-modal="showModal = false" />
 </template>
---
+++ src/components/Modals/AddItemModal.vue
@@ -0,0 +1,81 @@
+<template>
+  <div
+    class="fixed flex justify-center top-0 bottom-0 left-0 right-0 bg-[#000000da]"
+    @click="$emit('close-modal')"
+  >
+    <div class="bg-white my-auto w-96 p-4" @click.stop>
+      <h3 class="text-2xl font-medium mb-4">Tambah Entri</h3>
+      <div
+        v-if="errorMsg.length > 0"
+        class="text-red-700 mb-2 font-medium text-center"
+      >
+        <p>{{ errorMsg }}</p>
+      </div>
+      <form class="space-y-4" @submit.prevent="submitForm">
+        <div class="flex flex-col">
+          <label for="name">Nama</label>
+          <input
+            id="name"
+            class="border p-1 rounded focus:ring-0 focus:border-2 focus:outline-none focus:border-indigo-500 active:border-indigo-800"
+            v-model="formData.name"
+          />
+        </div>
+        <div class="flex flex-col">
+          <label for="price">Harga</label>
+          <input
+            id="price"
+            class="border p-1 rounded focus:ring-0 focus:border-2 focus:outline-none focus:border-indigo-500 active:border-indigo-800"
+            type="number"
+            v-model="formData.price"
+          />
+        </div>
+      </form>
+      <div class="flex justify-end space-x-2 mt-4">
+        <button class="p-2 bg-gray-500 text-white" @click="closeModal">
+          BATAL
+        </button>
+        <button
+          type="submit"
+          class="p-2 bg-indigo-800 text-white"
+          @click="submitForm"
+        >
+          KIRIM
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        name: "",
+        price: "",
+      },
+      errorMsg: "",
+    };
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close-modal");
+      this.resetForm();
+    },
+    submitForm() {
+      this.errorMsg = "";
+      if (this.formData.name.length === 0 || this.formData.price.length === 0) {
+        this.errorMsg = "Mohon isi semua form";
+      } else {
+        // Execute submit form
+        this.resetForm();
+      }
+    },
+    resetForm() {
+      this.formData.name = "";
+      this.formData.price = "";
+      this.errorMsg = "";
+    },
+  },
+};
+</script>
